46c71dd113dfdb22459fa3deefd6d7943d789cf4
RochelLevi <rochlevi@gmail.com>
2017-11-26 09:12:54 -0500

    done, but not passing all tests because I have add new owner as one of the radio buttons and Capybara doesn't know to click the radio button

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,45 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
+
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if !params[:owner_name].empty?
+      @pet.owner = Owner.create(name: params[:owner_name])
+      @pet.save
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
 
+    if params[:pet][:owner_id] == "on"
+      @pet.name = (params[:pet][:name])
+      @pet.owner = Owner.create(name: params[:owner][:name])
+      @pet.save
+
+    else
+      @pet.update(params[:pet])
+    end
+    
     redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id].to_i)
+    erb :'/pets/edit'
   end
 end
